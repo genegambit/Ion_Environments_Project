21fedfd22fccfc12be422f31624e555e5bc5fa20
Yogesh Joshi <yjoshi1984@gmail.com>
2018-05-08 13:49:20 -0700

    GitKraken
    Demo commit for GitKraken.

--- amino_dict.py
+++ amino_dict.py
@@ -1,16 +1,19 @@
 
 # REQUIRED DICTIONARY.
+# Three letter code as Keys and One Letter codes as values.
 one_letter ={'VAL':'V', 'ILE':'I', 'LEU':'L', 'GLU':'E', 'GLN':'Q', \
 'ASP':'D', 'ASN':'N', 'HIS':'H', 'TRP':'W', 'PHE':'F', 'TYR':'Y',    \
 'ARG':'R', 'LYS':'K', 'SER':'S', 'THR':'T', 'MET':'M', 'ALA':'A',    \
 'GLY':'G', 'PRO':'P', 'CYS':'C'}
 
+# Function for replacing a string based on 
+# key value pairs in dictionary.
 def replace_all(text, dic):
     for i, j in dic.items():
         text = text.replace(i, j)
     return text
 
-
+# List of one letter amino acid codes.
 amino = ['V','I','L','E','Q','D','N','H','W','F','Y','R','K','S','T','M','A','G','P','C']
 
 
